80a1ba0d30737fc00f1742b0d59368d6e3a4ccac
Kyle <techgamer@comcast.net>
2013-10-28 11:53:41 -0500

    Rendering. Movement with arrow keys. Collision.

--- IntroGameDev/IntroGameDev/main.cpp
+++ IntroGameDev/IntroGameDev/main.cpp
@@ -6,6 +6,8 @@
 #include <iostream>
 #include <stdio.h>
 #include <string>
+#include <time.h>
+#include <vector>
 
 
 #pragma comment(lib, "SDL2.lib")
@@ -19,6 +21,30 @@
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 600;
 
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+SDL_Surface* screenSurface = NULL;
+SDL_Surface* backgroundImage = NULL;
+SDL_Surface* ballImage = NULL;
+
+struct Circle
+{
+	int x, y, r;
+};
+
+class PVector
+{
+public: 
+	float x;
+	float y;
+
+	PVector(float x_, float y_)
+	{
+		x = x_;
+		y = y_;
+	}
+};
+
 class LTexture
 {
 public:
@@ -28,7 +54,8 @@ public:
 	bool loadFromFile(std::string path);
 
 	void free();
-	void Render(int x, int y);
+	void Render(int x, int y, SDL_Rect* clip = NULL, double angle = 0.0, SDL_Point* center = NULL,
+		SDL_RendererFlip = SDL_FLIP_NONE);
 
 	int getWidth();
 	int getHeight();
@@ -41,14 +68,42 @@ private:
 
 };
 
-SDL_Window* window = NULL;
-SDL_Renderer* renderer = NULL;
-SDL_Surface* screenSurface = NULL;
-SDL_Surface* backgroundImage = NULL;
-SDL_Surface* ballImage = NULL;
-
 LTexture backgroundTexture;
 LTexture ballTexture;
+
+int BALL_VELOCITY = 5;
+
+class Ball
+{
+public:
+	static const int BALL_WIDTH = 20;
+	static const int BALL_HEIGHT = 20;
+
+	Ball(int x, int y);
+
+	void HandleInput(SDL_Event& e);
+
+	void move(SDL_Rect& square, Circle& circle);
+
+	void Render_Ball();
+
+	Circle& getCollider();
+
+private:
+	int mPosX, mPosY;
+	int mVelX, mVelY;
+
+	Circle mCollider;
+
+	void shiftColliders();
+
+};
+
+bool checkCollision(Circle& objA, Circle& objB);
+
+bool checkCollision(Circle& objA, SDL_Rect& objB);
+
+double distanceSquared(int x1, int x2, int y1, int y2);
 
 LTexture::LTexture()
 {
@@ -75,7 +130,7 @@ bool LTexture::loadFromFile(std::string path)
 	}
 	else
 	{
-		SDL_SetColorKey(loadedSurface, SDL_TRUE, SDL_MapRGB(loadedSurface->format, 0, 0xFF, 0xFF));
+		SDL_SetColorKey(loadedSurface, SDL_TRUE, SDL_MapRGB(loadedSurface->format, 0xFF, 0xFF, 0xFF));
 
 		newTexture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
 		if(newTexture == NULL)
@@ -108,10 +163,17 @@ void LTexture::free()
 	}
 }
 
-void LTexture::Render(int xPos, int yPos)
+void LTexture::Render(int xPos, int yPos, SDL_Rect* clip, double angle, SDL_Point* center, SDL_RendererFlip flip)
 {
 	SDL_Rect renderQuad = {xPos, yPos, mWidth, mHeight};
-	SDL_RenderCopy(renderer, mTexture, NULL, &renderQuad);
+
+	if(clip != NULL)
+	{
+		renderQuad.w = clip->w;
+		renderQuad.h = clip->h;
+	}
+
+	SDL_RenderCopyEx(renderer, mTexture, clip, &renderQuad, angle, center, flip);
 }
 
 int LTexture::getWidth()
@@ -122,6 +184,158 @@ int LTexture::getWidth()
 int LTexture::getHeight()
 {
 	return mHeight;
+}
+
+Ball::Ball(int x, int y)
+{
+	mPosX = x;
+	mPosY = y;
+
+	mCollider.r = BALL_WIDTH / 2;
+
+	mVelX = 0;
+	mVelY = 0;
+
+	shiftColliders();
+}
+
+void Ball::HandleInput(SDL_Event& e)
+{
+	if(e.type == SDL_KEYDOWN && e.key.repeat == 0)
+	{
+		switch(e.key.keysym.sym)
+		{
+		case SDLK_UP: mVelY -= BALL_VELOCITY; 
+			break;
+		case SDLK_DOWN: mVelY += BALL_VELOCITY; 
+			break;
+		case SDLK_LEFT: mVelX -= BALL_VELOCITY; 
+			break;
+		case SDLK_RIGHT: mVelX += BALL_VELOCITY; 
+			break;
+		case SDLK_m: BALL_VELOCITY = 10;
+			break;
+		}
+	}
+
+	else if(e.type == SDL_KEYUP && e.key.repeat == 0)
+	{
+		switch(e.key.keysym.sym)
+		{
+		case SDLK_UP: mVelY += BALL_VELOCITY; 
+			break;
+		case SDLK_DOWN: mVelY -= BALL_VELOCITY; 
+			break;
+		case SDLK_LEFT: mVelX += BALL_VELOCITY; 
+			break;
+		case SDLK_RIGHT: mVelX -= BALL_VELOCITY; 
+			break;
+		case SDLK_m: BALL_VELOCITY = 5;
+			break;
+		}
+	}
+}
+
+void Ball::move(SDL_Rect& square, Circle& circle)
+{
+	mPosX += mVelX;
+	shiftColliders();
+
+	if((mPosX - BALL_WIDTH / 2 < 0) || (mPosX + BALL_WIDTH / 2 > WINDOW_WIDTH) || 
+		checkCollision(mCollider, square) || checkCollision(mCollider, circle))
+	{
+		mPosX -= mVelX;
+		shiftColliders();
+	}
+
+	mPosY += mVelY;
+	shiftColliders();
+
+	if((mPosY - BALL_HEIGHT / 2 < 0) || (mPosY + BALL_HEIGHT / 2 > WINDOW_HEIGHT) ||
+		checkCollision(mCollider, square) || checkCollision(mCollider, circle))
+	{
+		mPosY -= mVelY;
+		shiftColliders();
+	}
+
+}
+
+void Ball::Render_Ball()
+{
+	ballTexture.Render(mPosX - mCollider.r, mPosY - mCollider.r);
+}
+
+Circle& Ball::getCollider()
+{
+	return mCollider;
+}
+
+void Ball::shiftColliders()
+{
+	mCollider.x = mPosX;
+	mCollider.y = mPosY;
+}
+
+bool checkCollision(Circle& objA, Circle& objB)
+{
+	int tRadiusSquared = objA.r + objB.r;
+	tRadiusSquared = tRadiusSquared * tRadiusSquared;
+
+	if(distanceSquared(objA.x, objA.y, objB.x, objB.y) < (tRadiusSquared))
+	{
+		return true;
+	}
+
+	return false;
+}
+
+bool checkCollision(Circle& objA, SDL_Rect& objB)
+{
+	//Closest point in collision box
+	int cX, cY;
+
+	//Closest x offset
+	if(objA.x < objB.x)
+	{
+		cX = objB.x;
+	}
+	else if (objA.x > (objB.x + objB.w))
+	{
+		cX = objB.x + objB.w;
+	}
+	else
+	{
+		cX = objA.x;
+	}
+
+	//Closest y offset
+	if(objA.y < objB.y)
+	{
+		cY = objB.y;
+	}
+	else if (objA.y > (objB.y + objB.h))
+	{
+		cY = objB.y + objB.h;
+	}
+	else
+	{
+		cY = objA.y;
+	}
+
+	if(distanceSquared(objA.x, objA.y, cX, cY) < objA.r * objA.r)
+	{
+		return true;
+	}
+
+	return false;
+
+}
+
+double distanceSquared(int x1, int y1, int x2, int y2)
+{
+	int deltaX = x2 - x1;
+	int deltaY = y2 - y1;
+	return deltaX * deltaX + deltaY * deltaY;
 }
 
 //Creates window and surface.
@@ -136,7 +350,12 @@ bool Initialize()
 	}
 	else
 	{
-		 window = SDL_CreateWindow("Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		if(!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1"))
+		{
+			printf("Warning: Linear texture filtering not enabled.");
+		}
+
+		window = SDL_CreateWindow("Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
 			WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
 
 		if(window == NULL)
@@ -156,6 +375,13 @@ bool Initialize()
 			else
 			{
 				SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
+				int imgFlags = IMG_INIT_PNG;
+				if(!(IMG_Init(imgFlags) &imgFlags))
+				{
+					printf("SDL_image couldn ot be initialized. SDL_image Error: %s\n",  IMG_GetError());
+					success = false;
+				}
 			}
 		}
 	}
@@ -193,6 +419,7 @@ void Close()
 	window = NULL;
 	renderer = NULL;
 
+	IMG_Quit();
 	SDL_Quit();
 }
 
@@ -233,6 +460,16 @@ int main()
 
 			SDL_Event event;
 
+			Ball ball (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2);
+			Ball otherBall(WINDOW_WIDTH / 4, WINDOW_HEIGHT / 4);
+
+			SDL_Rect wall;
+
+			wall.x = 500;
+			wall.y = 150;
+			wall.w = 200;
+			wall.h = 20;
+
 			while(run)
 			{
 				while(SDL_PollEvent(&event) != 0)
@@ -252,11 +489,19 @@ int main()
 						}
 					}
 
+					ball.HandleInput(event);
+
+					ball.move(wall, otherBall.getCollider());
+
 					SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
 					SDL_RenderClear(renderer);
 
-					backgroundTexture.Render(0,0);
-					ballTexture.Render(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2);
+					SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF);
+					SDL_RenderDrawRect(renderer, &wall);
+
+
+					ball.Render_Ball();
+					otherBall.Render_Ball();
 
 					SDL_RenderPresent(renderer);
 				}
@@ -267,8 +512,6 @@ int main()
 	}
 
 	Close();
-
-	system("PAUSE");
 
 	return 0;
 }//end main
